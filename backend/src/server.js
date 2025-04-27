74c6a35c229671d14e1de173283b296334cb4e65
Luan.RAr <luanlra002@gmail.com>
2025-04-27 17:05:55 -0300

    conexao com bd

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const bd = require('../bd.js');
 
 const PORT = process.env.PORT;
-const pool = bd.connect();
 
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`);
