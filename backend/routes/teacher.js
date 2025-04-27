f3739c98388756561f9b323ef7d35729d0978f52
Luan.RAr <luanlra002@gmail.com>
2025-04-27 13:22:47 -0300

    organizando codigo

--- backend/routes/teacher.js
+++ backend/routes/teacher.js
@@ -3,22 +3,22 @@ const routerTeacher = express.Router();
 const teacherController = require('../controller/teacherController.js');
 
 //Login
-routerTeacher.post('/teacher/login', teacherController.login);
+routerTeacher.post('/teacher/login', teacherController.Login);
 
 //Cadastro
-routerTeacher.post('/teacher', teacherController.cadastrar);
+routerTeacher.post('/teacher', teacherController.Create);
 
 //Perfil
-routerTeacher.get('/teacher/:id', teacherController.verPerfil);
+routerTeacher.get('/teacher/:id', teacherController.GetProfile);
 
 //Home
-routerTeacher.get('/teacher/:id/home', teacherController.home);
+routerTeacher.get('/teacher/:id/home', teacherController.Home);
 
 //Deletar
-routerTeacher.delete('/teacher/:id', teacherController.deletar);
+routerTeacher.delete('/teacher/:id', teacherController.Delete);
 
 //Recuperar senha
-routerTeacher.post('/teacher/recuperar-senha', teacherController.recuperarSenha);
+routerTeacher.post('/teacher/recuperar-senha', teacherController.RecoverPassword);
 
 //Enviar chave publica
 routerTeacher.get('/teacher/public-key', teacherController.GetPublicKey);  
