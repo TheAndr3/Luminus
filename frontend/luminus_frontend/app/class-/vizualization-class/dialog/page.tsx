b5a9a3c2e469888a60006b195b04b9460c66403a
Armaaaaaaaaaaaaaaaaaaaaaaaaaaando <clashroyaleferraz@gmail.com>
2025-05-04 12:56:52 -0300

    style: terminando design da tela de cadastro de turma

--- frontend/luminus_frontend/app/class-/vizualization-class/dialog/page.tsx
+++ frontend/luminus_frontend/app/class-/vizualization-class/dialog/page.tsx
@@ -2,8 +2,13 @@
 import { ImportCSVButton } from "@/components/button-csv/import-csv-button"; // Importa o botão para importar CSV
 import { BaseInput } from "@/components/inputs/BaseInput"; // Importa o componente de input customizado
 import { Button } from "@/components/ui/button"; // Importa o botão customizado
-import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"; // Importa componentes do Dialog
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogOverlay, DialogTitle, DialogTrigger } from "@/components/ui/dialog"; // Importa componentes do Dialog
 import { use, useState } from "react"; // Importa hooks do React
+
+import { Pencil } from "lucide-react";
+import class_icon from "@/components/icon/icon_turma.svg" // Importa o ícone da turma em formato SVG
+import Image from "next/image"; // Importa o componente Image do Next.js para usar imagens de forma otimizada
+
 
 export default function dialogPage() {
     const [save, setSave] = useState(false); // Estado para controlar se os dados estão sendo salvos
@@ -15,21 +20,26 @@ export default function dialogPage() {
     const [inputInst, setinputInst] = useState('') // Estado para armazenar o valor do input Instituição
     const [inputPer, setInputPer] = useState('') // Estado para armazenar o valor do input Período
 
+    let title = "Digite o nome da turma"
+    const [titulo, setTitulo] = useState(title) // Estado para armazenar o título (nome da turma)
+    const [editing, setEditing] = useState(false); // Estado para controlar se o título está sendo editado
+
+
     // Função que reseta os campos dos inputs quando o dialog é fechado
     const handleDialogClose = () => {
         setInputDisc(''); // Reseta o valor do input Disciplina
         setInputPer(''); // Reseta o valor do input Período
         setinputInst(''); // Reseta o valor do input Instituição
+        setTitulo(title); // Reseta o título para o valor padrão
     }
 
     // Função chamada ao clicar no botão de "Concluir"
     const handleClick = () => {
-        // Simula evento de salvamento (apenas para mostrar uma animação de "salvando")
         setSave(true); // Marca que o salvamento está em andamento
         setTimeout(() => setSave(false), 3000); // Desmarca o salvamento após 3 segundos
 
         // Verifica se os campos Disciplina e Período estão preenchidos
-        if (inputDisc && inputPer) {
+        if (inputDisc && inputPer && titulo != title) {
             setOpen(false); // Fecha o dialog se os campos estiverem preenchidos
             alert("Dados salvos com sucesso!"); // Exibe alerta de sucesso
             handleDialogClose(); // Reseta os campos dos inputs
@@ -37,6 +47,7 @@ export default function dialogPage() {
             alert("Por favor, preencha os campos!"); // Exibe alerta se algum campo obrigatório não foi preenchido
         }
     }
+
 
     return (
         <>
@@ -50,55 +61,79 @@ export default function dialogPage() {
                 <DialogTrigger>
                     <Button> Abrir dialog test </Button> {/* Botão para abrir o dialog */}
                 </DialogTrigger>
+                <DialogOverlay className="fixed inset-0 bg-gray-400/10 backdrop-blur-xs" /> {/* Overlay com fundo e desfoque */}
+
                 <DialogContent>
-                    <DialogContent className="max-w-3xl bg-[#012D48] rounded-2xl text-white">
-                    
-                        <h2 className="text-2xl font-bold text-center mb-6">
-                            Digite o nome da turma ✏️
-                        </h2>
+                    <DialogContent className="max-w-4xl bg-[#012D48] rounded-2xl text-white border-1 border-black">
+
+                        {/* Header do dialog */}
+                        <div className="flex items-center gap-2 justify-center mb-6">
+                            <div className=" bg-white w-12 h-12 rounded-lg ">
+                                <Image 
+                                    src={class_icon} // Ícone da turma
+                                    alt="icone turma"
+                                    className="w-12 h-12 bg-white-200">
+                                </Image>
+                            </div>
+                            
+                            {editing ? (
+                                <input 
+                                    className="text-2xl font-bold border: 2px solid white" 
+                                    value={titulo}
+                                    onChange={(e) => setTitulo(e.target.value)} // Atualiza o valor do título
+                                />
+                            ) : (
+                                <span className="text-2xl font-bold"> {titulo} </span> // Exibe o título
+                            )}
+                            <Pencil className="w-5 h-5 text-white cursor-pointer" onClick={() => {setEditing(!editing)}}/> {/* Ícone de edição */}
+                        </div>
 
                         {/* Campos de input */}
-                        <div className="grid grid-cols-2 gap-4 mb-6">
-                            <div>
-                                <label className="text-sm">Disciplina:</label>
-                                {/* Campo de input para Disciplina */}
+                        <div className="grid grid-cols-2 gap-4 m-6 ">
+                            <div className="flex items-center gap-3 "> 
+                                <label className="text-sm">Disciplina:</label> {/* Label para o campo Disciplina */}
                                 <BaseInput 
+                                    className="w-70 text-gray-900 font-medium bg-gray-100 text-gray-700 rounded-lg "
                                     placeholder="Ex: EXA 702 - TPO1" 
-                                    value={inputDisc}
-                                    onChange={(e) => setInputDisc(e.target.value)} // Atualiza o estado com o valor do input
-                                ></BaseInput>
+                                    value={inputDisc} // Valor do input Disciplina
+                                    onChange={(e) => setInputDisc(e.target.value)} // Atualiza o valor do input
+                                />
                             </div>
-                            <div>
-                                <label className="text-sm">Instituição:</label>
-                                {/* Campo de input para Instituição */}
+
+                            <div className="flex items-center gap-3 ml-8">
+                                <label className="text-sm">Instituição:</label> {/* Label para o campo Instituição */}
                                 <BaseInput
+                                    className="w-70 text-gray-900 font-medium bg-gray-100 text-gray-700 rounded-lg "
                                     placeholder="Insira o nome da instituição (opcional)"
-                                    value={inputInst}
-                                    onChange={(e) => setinputInst(e.target.value)} // Atualiza o estado com o valor do input
-                                ></BaseInput>
+                                    value={inputInst} // Valor do input Instituição
+                                    onChange={(e) => setinputInst(e.target.value)} // Atualiza o valor do input
+                                />
                             </div>
-                            <div>
-                                <label className="text-sm">Período:</label>
-                                {/* Campo de input para Período */}
+
+                            <div className="flex items-center gap-5">
+                                <label className="text-sm">Período:</label> {/* Label para o campo Período */}
                                 <BaseInput 
+                                    className="w-70 text-gray-900 font-medium bg-gray-100 text-gray-700 rounded-lg "
                                     placeholder="23.2"
-                                    value={inputPer}
-                                    onChange={(e) => setInputPer(e.target.value)} // Atualiza o estado com o valor do input
-                                ></BaseInput>
+                                    value={inputPer} // Valor do input Período
+                                    onChange={(e) => setInputPer(e.target.value)} // Atualiza o valor do input
+                                />
                             </div>
-                            <div className="flex justify-end">
-                                <label className="text-sm">Nenhum arquivo: </label>
-                                {/* Botão para importar CSV */}
+
+                            {/* Botão para importar CSV */}
+                            <div className="flex items-center justify-end gap-2">
+                                <label className="text-sm">Nenhum arquivo: </label> 
                                 <ImportCSVButton/>
                             </div>
                         </div>
 
                         {/* Botão "Concluir" */}
-                        <div className="flex justify-end">
-                            <Button onClick={handleClick} className="bg-gray-200 text-black hover:bg-gray-300 rounded-full px-4 py-2">
+                        <div className="flex justify-end mr-7">
+                            <Button onClick={handleClick} className="bg-gray-300 text-black hover:bg-gray-400 rounded-full px-2 py-1 h-7">
                                 Concluir
                             </Button>
                         </div>
+
                     </DialogContent>
                 </DialogContent>
             </Dialog>
--- frontend/luminus_frontend/components/button-csv/import-csv-button.tsx
+++ frontend/luminus_frontend/components/button-csv/import-csv-button.tsx
@@ -16,7 +16,7 @@ export function ImportCSVButton() {
         ref={inputRef}
         className="hidden"
       />
-      <Button onClick={handleClick} className="bg-gray-200 text-black hover:bg-gray-300 rounded-full px-4 py-2">
+      <Button onClick={handleClick} className="bg-gray-300 text-black hover:bg-gray-400 rounded-full px-2 py-1 h-7">
         Importar CSV
       </Button>
     </>
