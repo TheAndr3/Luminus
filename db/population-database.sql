abdf48679f9abaf2d8be6420b6d48f0a360a502f
Maike de Oliveira Nascimento <maikoy606@hotmail.com>
2025-05-09 06:01:15 -0300

    feat: add population file

---
+++ db/population-database.sql
@@ -0,0 +1,10 @@
+--instituicoes
+insert into Institution(name, password, institution_email) values ("UEFS", "123", "uefs@uefs.com");
+insert into Institution(name, password, institution_email) values ("UFG", "123", "ufg@ufg.com");
+insert into Institution(name, password, institution_email) values ("UFU", "123", "ufu@ufu.com");
+
+--professores
+insert into Professor(name, password, professor_email) values ("tiago", "123", "tiago@gmail.com");
+insert into Professor(name, password, professor_email) values ("joao", "123", "joao@gmail.com");
+insert into Professor(name, password, professor_email) values ("maria", "123", "maria@gmail.com");
+
