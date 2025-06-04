a84c423fd08a22e047df4cee9df99f5500661e02
Luan.RAr <luanlra002@gmail.com>
2025-06-04 16:08:34 -0300

    Adicao de .env ao frontend e atualização do dossier serivices

--- frontend/luminus_frontend/services/api.ts
+++ frontend/luminus_frontend/services/api.ts
@@ -1,7 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
-const API_URL = `http://localhost:8081`;
+const API_PORT = process.env.NEXT_PUBLIC_API_PORT;
+console.log(API_PORT);
+const API_URL = `http://localhost:${API_PORT}`;
 
-export const api = axios.create( {baseURL: `${API_URL}`});
-
-
+export const api = axios.create({
+  baseURL: API_URL,
+});
