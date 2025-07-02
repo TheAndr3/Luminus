4a7ac333afeb5fbdd724eaf1810847c90b263fa4
Luan.RAr <luanlra002@gmail.com>
2025-07-02 00:43:07 -0300

    API na url

--- frontend/luminus_frontend/services/api.ts
+++ frontend/luminus_frontend/services/api.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
+const API_IP = process.env.NEXT_PUBLIC_API_IP;
 const API_PORT = process.env.NEXT_PUBLIC_API_PORT;
-console.log(API_PORT);
-const API_URL = `http://localhost:${API_PORT}`;
+
+const API_URL = `http://${API_IP}:${API_PORT}`;
 
 export const api = axios.create({
   baseURL: API_URL,
