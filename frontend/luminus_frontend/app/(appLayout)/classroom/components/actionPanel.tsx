8e9690c2833b612be42e85f66cd48f5a4b26851f
Daniel Lucas <danlucas2002@gmail.com>
2025-07-01 22:25:59 -0300

    Fix: Visual do actionPanel de turmas corrigido

--- frontend/luminus_frontend/app/(appLayout)/classroom/components/actionPanel.tsx
+++ frontend/luminus_frontend/app/(appLayout)/classroom/components/actionPanel.tsx
@@ -9,7 +9,7 @@ interface ActionPanelProps {
 export default function ActionPanel({ onDeleted }: ActionPanelProps) {
     return (
         <div className="left-4 bg-gray-900 shadow-lg rounded-xl flex flex-row gap-4 z-50 w-fit">
-            <button className="hover:bg-[#0A2B3D] cursor-pointer p-2 rounded-xl">
+            <button className="hover:bg-gray-800 cursor-pointer p-2 rounded-xl">
                 <Trash2
                     className="w-6 h-6 text-white"
                     onClick={onDeleted}
